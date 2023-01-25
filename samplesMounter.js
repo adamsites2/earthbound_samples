05ea54901ee44966f0c46b984c918a3091485552
adamsites2 <adamsites6@gmail.com>
2023-01-25 20:57:55 +0200

    finally i could load the xml thing

--- samplesMounter.js
+++ samplesMounter.js
@@ -1,5 +1,6 @@
 "use strict";
-fetch("/samples.xml").then(xmlSamples => xmlSamples.json().then(response => {
+fetch("/samples.xml").then(xmlSamples => xmlSamples.text().then(response => {
     const domParser = new DOMParser();
     const samplesDoc = domParser.parseFromString(response, "text/xml");
+    console.log(response, samplesDoc);
 }));
--- samplesMounter.ts
+++ samplesMounter.ts
@@ -1,4 +1,5 @@
-fetch("/samples.xml").then(xmlSamples => xmlSamples.json().then(response => {
+fetch("/samples.xml").then(xmlSamples => xmlSamples.text().then(response => {
     const domParser = new DOMParser()
     const samplesDoc = domParser.parseFromString(response, "text/xml")
+    console.log(response, samplesDoc)
 }))
