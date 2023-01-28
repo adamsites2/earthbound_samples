a87fdecf4a986eac26bd5623764793d82026cc55
adamsites2 <adamsites6@gmail.com>
2023-01-28 19:00:48 +0200

    ok

--- samplesMounter.js
+++ samplesMounter.js
@@ -1,11 +1,6 @@
 "use strict";
-function br() {
-    let br = document.createElement("span");
-    br.innerHTML = "<br/>";
-    return br;
-}
 fetch("/samples.xml").then(xmlSamples => xmlSamples.text().then(response => {
-    var _a;
+    var _a, _b, _c, _d, _e, _f, _g, _h;
     const samplesDiv = document.getElementById("samples");
     const domParser = new DOMParser();
     const samplesDocc = domParser.parseFromString(response, "text/xml");
@@ -13,15 +8,35 @@ fetch("/samples.xml").then(xmlSamples => xmlSamples.text().then(response => {
     const samples = samplesDoc.getElementsByTagName("sample");
     for (let i = 0; i < samples.length; i++) {
         const sample = samples.item(i);
-        let tableElement = document.createElement("tr");
-        const sampleIndexes = (_a = sample.textContent) === null || _a === void 0 ? void 0 : _a.split("\\\\\\\\");
-        sampleIndexes.forEach((index) => {
-            let sampledName = document.createElement("td");
-            sampledName.textContent = index;
-            tableElement.appendChild(sampledName);
-            tableElement.appendChild(br());
+        let tableElement = document.createElement("li");
+        const sampledIndexes = (_b = (_a = sample.getElementsByTagName("indexMusic").item(0)) === null || _a === void 0 ? void 0 : _a.innerHTML) === null || _b === void 0 ? void 0 : _b.split("\\\\\\\\");
+        let subTable = document.createElement("table");
+        subTable.classList.add("fixed");
+        tableElement.appendChild(subTable);
+        let trElement = document.createElement("tr");
+        subTable.appendChild(trElement);
+        let colGroup = document.createElement("colgroup");
+        let colWidth = document.createElement("col");
+        colWidth.width = ((_c = sample.getElementsByTagName("colWidth").item(0)) === null || _c === void 0 ? void 0 : _c.textContent) || "600px";
+        colGroup.appendChild(colWidth);
+        subTable.appendChild(colGroup);
+        let sampledName = document.createElement("td");
+        sampledIndexes.forEach((index) => {
+            sampledName.innerHTML = sampledName.innerHTML + index + " ";
         });
-        console.log(tableElement);
+        subTable.appendChild(sampledName);
         samplesDiv.appendChild(tableElement);
+        const samplesIndexes = (_d = sample.getElementsByTagName("sampleValues").item(0)) === null || _d === void 0 ? void 0 : _d.getElementsByTagName("sampleValues");
+        let samplesName = document.createElement("td");
+        for (let index = 0; i < samplesIndexes.length; i++) {
+            let value = samplesIndexes.item(index);
+            let sampleMusic = (_e = value.getElementsByTagName("index").item(0)) === null || _e === void 0 ? void 0 : _e.textContent;
+            let sampleTimestamp = (_f = value.getElementsByTagName("timestamp").item(0)) === null || _f === void 0 ? void 0 : _f.textContent;
+            let sampleCreator = (_g = value.getElementsByTagName("creator").item(0)) === null || _g === void 0 ? void 0 : _g.textContent;
+            let constructedHtmlLi = `<li>${sampleMusic}</br>${sampleTimestamp}</br>${sampleCreator}</li>`;
+            samplesName.innerHTML = samplesName.innerHTML + constructedHtmlLi + " ";
+        }
+        trElement.appendChild(samplesName);
+        tableElement.appendChild(document.createTextNode((_h = sample.getElementsByTagName("description").item(0)) === null || _h === void 0 ? void 0 : _h.textContent));
     }
 }));
--- samplesMounter.ts
+++ samplesMounter.ts
@@ -1,8 +1,3 @@
-function br(){
-    let br = document.createElement("span");
-    br.innerHTML = "<br/>";
-    return br
-}
 fetch("/samples.xml").then(xmlSamples => xmlSamples.text().then(response => {
     const samplesDiv = document.getElementById("samples") as HTMLDivElement
     const domParser = new DOMParser()
@@ -10,16 +5,36 @@ fetch("/samples.xml").then(xmlSamples => xmlSamples.text().then(response => {
     const samplesDoc = samplesDocc.getElementById("samples") as HTMLDivElement
     const samples = samplesDoc.getElementsByTagName("sample")
     for (let i = 0; i < samples.length; i++){
-        const sample = samples.item(i) as HTMLElement
-        let tableElement = document.createElement("tr") as HTMLTableRowElement
-        const sampleIndexes = sample.textContent?.split("\\\\\\\\") as string[]
-        sampleIndexes.forEach((index) => {
-            let sampledName = document.createElement("td") as HTMLTableCellElement
-            sampledName.textContent = index
-            tableElement.appendChild(sampledName)
-            tableElement.appendChild(br())
+        const sample = samples.item(i) as HTMLDivElement
+        let tableElement = document.createElement("li") as HTMLLIElement
+        const sampledIndexes = sample.getElementsByTagName("indexMusic").item(0)?.innerHTML?.split("\\\\\\\\") as string[]
+        let subTable = document.createElement("table") as HTMLTableElement
+        subTable.classList.add("fixed")
+        tableElement.appendChild(subTable)
+        let trElement = document.createElement("tr") as HTMLTableRowElement
+        subTable.appendChild(trElement)
+        let colGroup = document.createElement("colgroup")
+        let colWidth = document.createElement("col")
+        colWidth.width = sample.getElementsByTagName("colWidth").item(0)?.textContent || "600px"
+        colGroup.appendChild(colWidth)
+        subTable.appendChild(colGroup)
+        let sampledName = document.createElement("td") as HTMLTableCellElement
+        sampledIndexes.forEach((index) => {
+            sampledName.innerHTML = sampledName.innerHTML + index + " "
         })
-        console.log(tableElement)
+        subTable.appendChild(sampledName)
         samplesDiv.appendChild(tableElement)
+        const samplesIndexes = sample.getElementsByTagName("sampleValues").item(0)?.getElementsByTagName("sampleValues") as HTMLCollection
+        let samplesName = document.createElement("td") as HTMLTableCellElement
+        for (let index = 0; i < samplesIndexes.length; i++){
+            let value = samplesIndexes.item(index) as HTMLElement
+            let sampleMusic = value.getElementsByTagName("index").item(0)?.textContent as string
+            let sampleTimestamp = value.getElementsByTagName("timestamp").item(0)?.textContent as string
+            let sampleCreator = value.getElementsByTagName("creator").item(0)?.textContent as string
+            let constructedHtmlLi = `<li>${sampleMusic}</br>${sampleTimestamp}</br>${sampleCreator}</li>`
+            samplesName.innerHTML = samplesName.innerHTML + constructedHtmlLi + " "
+        }
+        trElement.appendChild(samplesName)
+        tableElement.appendChild(document.createTextNode(sample.getElementsByTagName("description").item(0)?.textContent as string))
     }
 }))
