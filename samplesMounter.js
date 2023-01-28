25a572e6fa9c31f98a8a6280a0da4b480c0b08c7
adamsites2 <adamsites6@gmail.com>
2023-01-28 17:03:32 +0200

    progress

--- samplesMounter.js
+++ samplesMounter.js
@@ -1,8 +1,14 @@
 "use strict";
 fetch("/samples.xml").then(xmlSamples => xmlSamples.text().then(response => {
+    const samplesDiv = document.getElementById("samples");
     const domParser = new DOMParser();
     const samplesDoc = domParser.parseFromString(response, "text/xml");
     const samples = samplesDoc.getElementsByName("sample");
     samples.forEach((sample) => {
+        var _a;
+        let tableElement = document.createElement("tr");
+        const sampleIndexes = (_a = sample.textContent) === null || _a === void 0 ? void 0 : _a.split("\n");
+        sampleIndexes.forEach((index) => {
+        });
     });
 }));
--- samplesMounter.ts
+++ samplesMounter.ts
@@ -1,8 +1,14 @@
 fetch("/samples.xml").then(xmlSamples => xmlSamples.text().then(response => {
+    const samplesDiv = document.getElementById("samples") as HTMLDivElement
     const domParser = new DOMParser()
     const samplesDoc = domParser.parseFromString(response, "text/xml")
     const samples = samplesDoc.getElementsByName("sample")
+    
     samples.forEach((sample) => {
-        
+        let tableElement = document.createElement("tr") as HTMLTableRowElement
+        const sampleIndexes = sample.textContent?.split("\n") as string[]
+        sampleIndexes.forEach((index) => {
+
+        })
     })
 }))
