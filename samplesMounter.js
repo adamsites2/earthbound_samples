5f5ae4dc8aff8eb4a2d3a1f5f1b0d074c0dacfca
adamsites2 <adamsites6@gmail.com>
2023-01-28 17:35:12 +0200

    fixed

--- samplesMounter.js
+++ samplesMounter.js
@@ -5,16 +5,16 @@ fetch("/samples.xml").then(xmlSamples => xmlSamples.text().then(response => {
     const domParser = new DOMParser();
     const samplesDocc = domParser.parseFromString(response, "text/xml");
     const samplesDoc = samplesDocc.getElementById("samples");
-    samplesDoc.innerHTML = samplesDoc.innerHTML.replace("</br>", "");
     const samples = samplesDoc.getElementsByTagName("sample");
     for (let i = 0; i < samples.length; i++) {
         const sample = samples.item(i);
         let tableElement = document.createElement("tr");
-        const sampleIndexes = (_a = sample.textContent) === null || _a === void 0 ? void 0 : _a.split("\n");
+        const sampleIndexes = (_a = sample.textContent) === null || _a === void 0 ? void 0 : _a.split("\\\\\\\\");
         sampleIndexes.forEach((index) => {
             let sampledName = document.createElement("td");
-            sampledName.textContent = index + "</br>";
+            sampledName.textContent = index;
             tableElement.appendChild(sampledName);
+            tableElement.appendChild(document.createElement("br"));
         });
         console.log(tableElement);
         samplesDiv.appendChild(tableElement);
--- samplesMounter.ts
+++ samplesMounter.ts
@@ -3,16 +3,16 @@ fetch("/samples.xml").then(xmlSamples => xmlSamples.text().then(response => {
     const domParser = new DOMParser()
     const samplesDocc = domParser.parseFromString(response, "text/xml")
     const samplesDoc = samplesDocc.getElementById("samples") as HTMLDivElement
-    samplesDoc.innerHTML = samplesDoc.innerHTML.replace("</br>", "")
     const samples = samplesDoc.getElementsByTagName("sample")
     for (let i = 0; i < samples.length; i++){
         const sample = samples.item(i) as HTMLElement
         let tableElement = document.createElement("tr") as HTMLTableRowElement
-        const sampleIndexes = sample.textContent?.split("\n") as string[]
+        const sampleIndexes = sample.textContent?.split("\\\\\\\\") as string[]
         sampleIndexes.forEach((index) => {
             let sampledName = document.createElement("td") as HTMLTableCellElement
-            sampledName.textContent = index + "</br>"
+            sampledName.textContent = index
             tableElement.appendChild(sampledName)
+            tableElement.appendChild(document.createElement("br"))
         })
         console.log(tableElement)
         samplesDiv.appendChild(tableElement)
