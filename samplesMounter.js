1df0574b12c33d138fb009bdbaca166c5b169c9a
adamsites2 <adamsites6@gmail.com>
2023-01-28 17:18:01 +0200

    ok

--- samplesMounter.js
+++ samplesMounter.js
@@ -1,11 +1,12 @@
 "use strict";
 fetch("/samples.xml").then(xmlSamples => xmlSamples.text().then(response => {
+    var _a;
     const samplesDiv = document.getElementById("samples");
     const domParser = new DOMParser();
-    const samplesDoc = domParser.parseFromString(response, "text/xml");
-    const samples = samplesDoc.getElementsByName("sample");
-    samples.forEach((sample) => {
-        var _a;
+    const samplesDoc = domParser.parseFromString(response, "text/xml").getElementsByName("samples")[0];
+    const samples = samplesDoc.getElementsByTagName("sample");
+    for (let i = 0; i < samples.length; i++) {
+        const sample = samples.item(i);
         let tableElement = document.createElement("tr");
         const sampleIndexes = (_a = sample.textContent) === null || _a === void 0 ? void 0 : _a.split("\n");
         sampleIndexes.forEach((index) => {
@@ -13,6 +14,7 @@ fetch("/samples.xml").then(xmlSamples => xmlSamples.text().then(response => {
             sampledName.textContent = index + "</br>";
             tableElement.appendChild(sampledName);
         });
+        console.log(tableElement);
         samplesDiv.appendChild(tableElement);
-    });
+    }
 }));
--- samplesMounter.ts
+++ samplesMounter.ts
@@ -1,10 +1,11 @@
 fetch("/samples.xml").then(xmlSamples => xmlSamples.text().then(response => {
     const samplesDiv = document.getElementById("samples") as HTMLDivElement
     const domParser = new DOMParser()
-    const samplesDoc = domParser.parseFromString(response, "text/xml")
-    const samples = samplesDoc.getElementsByName("sample")
+    const samplesDoc = domParser.parseFromString(response, "text/xml").getElementsByName("samples")[0] as HTMLDivElement
+    const samples = samplesDoc.getElementsByTagName("sample")
     
-    samples.forEach((sample) => {
+    for (let i = 0; i < samples.length; i++){
+        const sample = samples.item(i) as HTMLElement
         let tableElement = document.createElement("tr") as HTMLTableRowElement
         const sampleIndexes = sample.textContent?.split("\n") as string[]
         sampleIndexes.forEach((index) => {
@@ -12,6 +13,7 @@ fetch("/samples.xml").then(xmlSamples => xmlSamples.text().then(response => {
             sampledName.textContent = index + "</br>"
             tableElement.appendChild(sampledName)
         })
+        console.log(tableElement)
         samplesDiv.appendChild(tableElement)
-    })
+    }
 }))
